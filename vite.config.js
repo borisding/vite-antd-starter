519cbe0b50c6c7df69d57b9f92cc8cf8c5a72546
borisding <boris.ding82@outlook.com>
2022-10-21 00:34:30 +0800

    import stylesheet manually. add theme folder

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vitePluginImp from 'vite-plugin-imp';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginHtmlEnv(),
-    vitePluginImp({
-      optimize: true,
-      libList: [
-        {
-          libName: 'antd',
-          libDirectory: 'es',
-          style: name => `antd/es/${name}/style`
-        }
-      ]
-    })
-  ],
+  plugins: [react(), VitePluginHtmlEnv()],
   css: {
     preprocessorOptions: {
       less: {
