7dc108b346cc84d3ed1a1ef4f072001d4e74a07b
borisding <boris.ding82@outlook.com>
2022-10-25 19:47:27 +0800

    reorganize style files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Button, Typography } from 'antd';
-import './App.less';
 
 const { Paragraph, Text } = Typography;
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.less';
+import 'antd/dist/antd.less';
+import '@/theme/global.less';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
